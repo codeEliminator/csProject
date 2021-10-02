774c0c9df3478a109a8d708ec02025148bb7790f
ManSap <mansap@email.com>
2021-10-03 02:12:19 +0300

    hp added and bugs fixed

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 "!use strict"
-
-// let crosshair = document.getElementsByClassName("crosshair")[0]
-
-// const crosshairMove = event => {
-//     crosshair.style.paddingLeft = Number(event.target.offsetX) + "px";
-//     crosshair.style.paddingTop = Number(event.target.offsetY) + "px";
-// }
-// container.addEventListener("mousemove", crosshairMove);
